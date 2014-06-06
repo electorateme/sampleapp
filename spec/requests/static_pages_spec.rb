c0786ba7ede25878eb1779c54d7b5c2f857ac2a8
Dan Haecker <dan@electorate.me>
2014-06-06 17:07:21 -0400

    Add tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,13 @@ describe "Static pages" do
       expect(page).to have_content('webapp')
     end
   end
+
+  describe "Help Page" do
+    
+    it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        expect.(page).to have_content('Help')
+    end
+end
+
 end
